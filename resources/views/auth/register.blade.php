ea5879fd703d53af6e1cbdc5370dbe92d9cac325
helloteam-developer1 <helloteam.developer1@gmail.com>
2022-09-19 19:29:07 -0500

    hola

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,14 +5,12 @@
                 <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
             </a>
         </x-slot>
-
-        <!-- Validation Errors -->
+        
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
         <form method="POST" action="{{ route('register') }}">
             @csrf
 
-            <!-- Name -->
             <div>
                 <x-input-label for="nombreusuario" :value="__('Nombre completo')" />
 
